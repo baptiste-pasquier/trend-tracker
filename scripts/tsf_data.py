cc34e028629b1163f0e14449a37c1e14670f6fae
hugodebes <hugo.debes@gmail.com>
2022-12-29 17:22:24 +0100

    init branch

---
+++ scripts/tsf_data.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer, KafkaConsumer
+import time
+import json
+from ingest_tweets import RAW_TOPIC
+
+def main():
+	#Call a Consumer to retrieve the raw tweets
+
+	#Preprocess the tweets 
+
+	#Send the preprocessed data 
+
+	
+
+if __name__ = "__main__":
+	main()
